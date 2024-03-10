c695414b889540232e3c735c928ba3233c19eb49
joycreations <115366687+joycreations@users.noreply.github.com>
2024-03-10 12:30:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ module.exports = {
 
 	//	These values will be ignored if you have set the environment variables (must be in uppercase)
 
-	prefix: 'm!', // required, command prefix
-	botToken: 'MTIxNjI3Nzk3MzMwOTg0OTY4MA.GZDgXB.BiFlV-KM9uX6c0OLeIUgA8wowFnR62NL3DxDBo', // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
-	clientId: '1216277973309849680', // optional, the bot's client ID, leave empty to disable slash commands
-	geniusApiToken: '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', // optional, but recommended for lyrics search - https://genius.com/api-clients
+	prefix: '', // required, command prefix
+	botToken: '', // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
+	clientId: '', // optional, the bot's client ID, leave empty to disable slash commands
+	geniusApiToken: '', // optional, but recommended for lyrics search - https://genius.com/api-clients
 
 	// still under development, so you should leave these empty
 	webplayer: '', // optional
